f3bd89edf7e797deb43ffc1a5b503584fb3a3f67
Ryan Silva <RyandotSilva@gmail.com>
2019-08-25 13:45:51 -0400

    Add Directory component

---
+++ client/src/components/Directory/Directory.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+import MenuItem from '../MenuItem/MenuItem';
+
+export default class Directory extends Component {
+	constructor(props) {
+		super(props);
+
+		
+}
