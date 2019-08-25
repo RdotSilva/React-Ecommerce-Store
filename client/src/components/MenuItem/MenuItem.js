0491f5005b3c3cd61597d62ed9c682dde6ec0414
Ryan Silva <RyandotSilva@gmail.com>
2019-08-25 14:21:40 -0400

    Add size to MenuItem

--- client/src/components/MenuItem/MenuItem.js
+++ client/src/components/MenuItem/MenuItem.js
@@ -2,12 +2,12 @@ import React from "react";
 
 import "./MenuItemStyles.scss";
 
-const MenuItem = ({ title, imageUrl }) => (
+const MenuItem = ({ title, imageUrl, size }) => (
 	<div
 		style={{
 			backgroundImage: `url(${imageUrl})`
 		}}
-		className="menu-item"
+		className={`${size} menu-item`}
 	>
 		<div className="content">
 			<div className="title">{title}</div>
