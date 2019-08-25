63ac1770e1c869cb0d8e25a363fd8dd844b746ea
Ryan Silva <RyandotSilva@gmail.com>
2019-08-25 13:35:28 -0400

    Add MenuItem component

---
+++ client/src/components/MenuItem/MenuItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MenuItem = ({ title }) => (
+	<div className="menu-item">
+		<div className="content">
+			<div className="title">{title}</div>
+			<span className="subtitle">SHOP NOW</span>
+		</div>
+	</div>
+);
+
+export default MenuItem;
