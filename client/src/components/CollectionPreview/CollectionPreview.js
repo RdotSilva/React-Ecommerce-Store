312938f8b2e43851761d502fce3c6fec32b86959
Ryan Silva <RyandotSilva@gmail.com>
2019-08-26 16:32:26 -0400

    Add map to display collection items

--- client/src/components/CollectionPreview/CollectionPreview.js
+++ client/src/components/CollectionPreview/CollectionPreview.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./CollectionPreviewStyles.scss";
 
-const CollectionPreview = () => {
+const CollectionPreview = ({ title, items }) => {
 	return (
 		<div className="collection-preview">
-			<h1 className="title">Title</h1>
-			<div className="preview"></div>
+			<h1 className="title">{title.toUpperCase()}</h1>
+			<div className="preview">
+				{items.map(item => (
+					<div key={item.id}>{item.name}</div>
+				))}
+			</div>
 		</div>
 	);
 };
