c19955cfe9c68c6adfc9b6b29f37cb5520e53969
Ryan Silva <RyandotSilva@gmail.com>
2019-08-26 16:26:25 -0400

    Add shop page to main app

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,14 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/Homepage/Homepage";
-
-const HatsPage = () => {
-	return <h1> Hats Page</h1>;
-};
+import ShopPage from "./pages/Shop/Shop";
 
 function App() {
 	return (
 		<div>
 			<Switch>
 				<Route exact path="/" component={HomePage} />
-				<Route path="/shop/hats" component={HatsPage} />
+				<Route path="/shop" component={ShopPage} />
 			</Switch>
 		</div>
 	);
