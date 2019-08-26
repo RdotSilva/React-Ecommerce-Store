e8f2f5e5f39fb521eb46675bdcdb7477b13334e4
Ryan Silva <RyandotSilva@gmail.com>
2019-08-26 16:03:09 -0400

    Add switch and routes to main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.css";
 
+import { Switch, Route } from "react-router-dom";
+
 import HomePage from "./pages/Homepage/Homepage";
 
 function App() {
-	return <HomePage></HomePage>;
+	return (
+		<div>
+			<Switch>
+				<Route exact path="/" component={Homepage} />
+				<Route path="/hats" component={HatsPage} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
