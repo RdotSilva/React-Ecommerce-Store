a55d1d3ddec2f280cf86f672de46e9e027e48324
Ryan Silva <RyandotSilva@gmail.com>
2019-08-26 16:12:18 -0400

    Add HatsPage placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,16 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/Homepage/Homepage";
 
+const HatsPage = () => {
+	return <h1> Hats Page</h1>;
+};
+
 function App() {
 	return (
 		<div>
 			<Switch>
 				<Route exact path="/" component={HomePage} />
-				<Route path="/hats" component={HatsPage} />
+				<Route path="/shop/hats" component={HatsPage} />
 			</Switch>
 		</div>
 	);
