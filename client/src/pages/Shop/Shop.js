809f373da7e36e159abf65885b0941f19f608b11
Ryan Silva <RyandotSilva@gmail.com>
2019-08-26 16:34:55 -0400

    Add collection preview map

--- client/src/pages/Shop/Shop.js
+++ client/src/pages/Shop/Shop.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import SHOP_DATA from "./ShopData";
 
+import CollectionPreview from "../../components/CollectionPreview/CollectionPreview";
+
 class Shop extends Component {
 	constructor(props) {
 		super(props);
@@ -11,7 +13,18 @@ class Shop extends Component {
 	}
 
 	render() {
-		return <div>SHOP PAGE</div>;
+		const { collections } = this.state;
+
+		return (
+			<div className="shop-page">
+				{collections.map(({ id, ...otherCollectionProps }) => (
+					<CollectionPreview
+						key={id}
+						{...otherCollectionProps}
+					></CollectionPreview>
+				))}
+			</div>
+		);
 	}
 }
 
