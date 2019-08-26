bdf17954c90d1fd6a6db4d6b94d245fc8740aa51
Ryan Silva <RyandotSilva@gmail.com>
2019-08-26 16:30:15 -0400

    Add class names

---
+++ client/src/components/CollectionPreview/CollectionPreview.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./CollectionPreviewStyles.scss";
+
+const CollectionPreview = () => {
+	return (
+		<div className="collection-preview">
+			<h1 className="title">Title</h1>
+			<div className="preview"></div>
+		</div>
+	);
+};
+
+export default CollectionPreview;
