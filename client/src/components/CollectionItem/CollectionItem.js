3aea444335ff20672e4cdb9fab0f24fa22caea12
Ryan Silva <RyandotSilva@gmail.com>
2019-08-27 17:50:05 -0400

    Add footer with namd and price

--- client/src/components/CollectionItem/CollectionItem.js
+++ client/src/components/CollectionItem/CollectionItem.js
@@ -5,7 +5,12 @@ import "./CollectionItemStyles.scss";
 const CollectionItem = ({ id, name, price, imageUrl }) => {
 	return (
 		<div className="collection-item">
-			<div className="image"></div>
+			<div className="image" style={{ backgroundImage: `url(${imageUrl})` }}>
+				<div className="collection-footer">
+					<span className="name">{name}</span>
+					<span className="price">{price}</span>
+				</div>
+			</div>
 		</div>
 	);
 };
