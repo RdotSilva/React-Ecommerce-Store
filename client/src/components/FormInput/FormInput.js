b34dff4a19fce1be63936b73bdf46d4c9f344e7b
Ryan Silva <RyandotSilva@gmail.com>
2019-08-28 19:40:20 -0400

    Add shrink logic for label class name

--- client/src/components/FormInput/FormInput.js
+++ client/src/components/FormInput/FormInput.js
@@ -6,6 +6,13 @@ const FormInput = ({ handleChange, label, ...otherProps }) => {
 	return (
 		<div className="group">
 			<input className="form-input" onChange={handleChange} {...otherProps} />
+			{label ? (
+				<label
+					className={`${props.value.length ? "shrink" : ""} form-input-label`}
+				>
+					{label}
+				</label>
+			) : null}
 		</div>
 	);
 };
