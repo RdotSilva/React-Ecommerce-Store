7cb52bffe3207b735e3e88bf561ca44994d72bc1
Ryan Silva <RyandotSilva@gmail.com>
2019-08-28 19:53:43 -0400

    Add button

--- client/src/components/CustomButton/CustomButton.js
+++ client/src/components/CustomButton/CustomButton.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./CustomButtonStyles.scss";
 
-const CustomButton = () => {
-	return <button className="custom-button"></button>;
+const CustomButton = ({ children, ...otherProps }) => {
+	return (
+		<button className="custom-button" {...otherProps}>
+			{children}
+		</button>
+	);
 };
 
 export default CustomButton;
