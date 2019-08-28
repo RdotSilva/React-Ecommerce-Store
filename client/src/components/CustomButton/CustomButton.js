320eb1b115d3a9ee20d5989248d56cfa319d8ee0
Ryan Silva <RyandotSilva@gmail.com>
2019-08-28 19:53:02 -0400

    Add CustomButton component

---
+++ client/src/components/CustomButton/CustomButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CustomButtonStyles.scss";
+
+const CustomButton = () => {
+	return <button className="custom-button"></button>;
+};
+
+export default CustomButton;
