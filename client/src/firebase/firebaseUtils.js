d357da5cbad8c587b1d736e626d3f43b2286efe1
Ryan Silva <RyandotSilva@gmail.com>
2019-08-29 16:31:24 -0400

    Add provider for google auth

--- client/src/firebase/firebaseUtils.js
+++ client/src/firebase/firebaseUtils.js
@@ -13,3 +13,8 @@ const config = {
 };
 
 firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
