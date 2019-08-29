9730d3ddd80d00681dbe7e31b2702c72c49e817b
Ryan Silva <RyandotSilva@gmail.com>
2019-08-29 18:02:58 -0400

    Add sign out logic

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../firebase/firebaseUtils";
+
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./HeaderStyles.scss";
 
-const Header = () => {
-	return (
-		<div className="header">
-			<Link to="/" className="logo-container">
-				<Logo className="logo"></Logo>
+const Header = ({ currentUser }) => (
+	<div className="header">
+		<Link className="logo-container" to="/">
+			<Logo className="logo" />
+		</Link>
+		<div className="options">
+			<Link className="option" to="/shop">
+				SHOP
 			</Link>
-			<div className="options">
-				<Link className="option" to="/shop">
-					SHOP
+			<Link className="option" to="/shop">
+				CONTACT
+			</Link>
+			{currentUser ? (
+				<div className="option" onClick={() => auth.signOut()}>
+					SIGN OUT
+				</div>
+			) : (
+				<Link className="option" to="/signin">
+					SIGN IN
 				</Link>
-				<Link className="option" to="/shop">
-					CONTACT
-				</Link>
-			</div>
+			)}
 		</div>
-	);
-};
+	</div>
+);
 
 export default Header;
