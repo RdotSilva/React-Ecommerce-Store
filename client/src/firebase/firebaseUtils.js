186e72a5d7e45fac1cc2258a81f8aec77f4dc57a
Ryan Silva <RyandotSilva@gmail.com>
2019-08-29 16:30:34 -0400

    Add config

--- client/src/firebase/firebaseUtils.js
+++ client/src/firebase/firebaseUtils.js
@@ -2,4 +2,14 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+const config = {
+	apiKey: "AIzaSyB5rrv4ghTk7WWfM_FIijkpTcqGsnubk34",
+	authDomain: "ecommerce-db-e666d.firebaseapp.com",
+	databaseURL: "https://ecommerce-db-e666d.firebaseio.com",
+	projectId: "ecommerce-db-e666d",
+	storageBucket: "",
+	messagingSenderId: "392590566883",
+	appId: "1:392590566883:web:913f90f8c9b2b4ab"
+};
+
 firebase.initializeApp(config);
