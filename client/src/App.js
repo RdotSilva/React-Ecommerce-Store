82e196653da6d74728451c2298df0a22394ad699
Ryan Silva <RyandotSilva@gmail.com>
2019-08-29 17:55:14 -0400

    Add currentUser prop to Header

--- client/src/App.js
+++ client/src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<Header />
+				<Header currentUser={this.state.currentUser} />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
