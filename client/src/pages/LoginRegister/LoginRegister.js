7f4dedab71fea27152db329c736066cb7f261733
Ryan Silva <RyandotSilva@gmail.com>
2019-08-30 15:48:38 -0400

    Add Register component

--- client/src/pages/LoginRegister/LoginRegister.js
+++ client/src/pages/LoginRegister/LoginRegister.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./LoginRegister.scss";
 
 import Login from "../../components/Login/Login";
+import Register from "../../components/Register/Register";
 
 const LoginRegister = () => {
 	return (
 		<div className="sign-in-and-sign-up">
 			<Login />
+			<Register />
 		</div>
 	);
 };
