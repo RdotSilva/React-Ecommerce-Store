30ac21a239b7bf157831208b90f12705313ec481
Ryan Silva <RyandotSilva@gmail.com>
2019-08-31 12:32:25 -0400

    Add button to add to cart inside of collection

--- client/src/components/CollectionItem/CollectionItem.js
+++ client/src/components/CollectionItem/CollectionItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import CustomButton from "../CustomButton/CustomButton";
 import "./CollectionItemStyles.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
@@ -13,6 +14,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
 				<span className="name">{name}</span>
 				<span className="price">{price}</span>
 			</div>
+			<CustomButton inverted>Add to card</CustomButton>
 		</div>
 	);
 };
