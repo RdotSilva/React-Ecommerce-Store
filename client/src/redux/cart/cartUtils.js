2f1b5ab843f2456c2bc88e611909d214403740b6
Ryan Silva <RyandotSilva@gmail.com>
2019-08-31 13:07:16 -0400

    Add cart util to increment cart item quantity

---
+++ client/src/redux/cart/cartUtils.js
@@ -0,0 +1,20 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+	// Check if cart item already exists
+	const existingCartItem = cartItems.find(
+		cartItem => cartItem.id === cartItemToAdd.id
+	);
+
+	// If cart item exists increment quantity by 1.
+	if (existingCartItem) {
+		return cartItems.map(cartItem =>
+			cartItem.id === cartItemToAdd.id
+				? { ...cartItem, quantity: cartItem.quantity + 1 }
+				: {
+						cartItem
+				  }
+		);
+	}
+
+	// If cart item doesn't exist add it and init quantity to 1.
+	return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
