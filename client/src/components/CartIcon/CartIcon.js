bf78f6bd16600679be84472b9e7de163b4691f8e
Ryan Silva <RyandotSilva@gmail.com>
2019-08-31 19:20:48 -0400

    Update mapStateToProps with selector

--- client/src/components/CartIcon/CartIcon.js
+++ client/src/components/CartIcon/CartIcon.js
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 
 import { toggleCartHidden } from "../../redux/cart/cartActions";
 
+import { selectCartItemsCount } from "../../redux/cart/cartSelectors";
+
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => {
@@ -19,11 +21,8 @@ const mapDispatchToProps = dispatch => ({
 	toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-	itemCount: cartItems.reduce(
-		(accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
-		0
-	)
+const mapStateToProps = state => ({
+	itemCount: selectCartItemsCount(state)
 });
 
 export default connect(
