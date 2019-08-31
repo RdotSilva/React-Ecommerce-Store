010afa3ec9abdd4bd4cb86421f0249a6a9a911a3
Ryan Silva <RyandotSilva@gmail.com>
2019-08-31 12:05:16 -0400

    Update mapStateToProps

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -10,7 +10,7 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./HeaderStyles.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
 	<div className="header">
 		<Link className="logo-container" to="/">
 			<Logo className="logo" />
@@ -37,8 +37,9 @@ const Header = ({ currentUser }) => (
 	</div>
 );
 
-const mapStateToProps = state => ({
-	currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+	currentUser,
+	hidden
 });
 
 export default connect(mapStateToProps)(Header);
