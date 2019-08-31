e136e8e0b35dc09a9ba0fcc464eefc412a76f058
Ryan Silva <RyandotSilva@gmail.com>
2019-08-31 11:39:29 -0400

    Add CartIcon component

---
+++ client/src/components/CartIcon/CartIcon.js
@@ -0,0 +1,9 @@
+import "./CartIcon.scss";
+import React from "react";
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+
+const CartIcon = () => {
+	return <div></div>;
+};
+
+export default CartIcon;
