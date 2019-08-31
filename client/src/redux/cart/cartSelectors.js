52251a3e4560715994371ab0788d3418c14f31fe
Ryan Silva <RyandotSilva@gmail.com>
2019-08-31 19:18:48 -0400

    Add selectCartItemsCount selector

--- client/src/redux/cart/cartSelectors.js
+++ client/src/redux/cart/cartSelectors.js
@@ -6,3 +6,13 @@ export const selectCartItems = createSelector(
 	[selectCart],
 	cart => cart.cartItems
 );
+
+export const selectCartItemsCount = createSelector(
+	[selectCartItems],
+	cartItems =>
+		cartItems.reduce(
+			(accumulatedQuantity, cartItem) =>
+				accumulatedQuantity + cartItem.quantity,
+			0
+		)
+);
