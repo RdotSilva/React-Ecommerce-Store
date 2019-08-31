e7161531d3e36d58eae37eede8fc11fad38fe481
Ryan Silva <RyandotSilva@gmail.com>
2019-08-31 10:07:51 -0400

    Add switch statement

--- client/src/redux/user/userReducer.js
+++ client/src/redux/user/userReducer.js
@@ -2,4 +2,14 @@ const initialState = {
 	currentUser: null
 };
 
-const userReducer = (state, action) => {};
+const userReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case "SET_CURRENT_USER":
+			return {
+				...state,
+				currentUser: action.payload
+			};
+		default:
+			return state;
+	}
+};
