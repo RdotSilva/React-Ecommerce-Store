5d8167527a35945d2d99ba883dcb2dbe8882ad7d
Ryan Silva <RyandotSilva@gmail.com>
2019-08-31 12:03:26 -0400

    Add destructuring for props

--- client/src/components/CartIcon/CartIcon.js
+++ client/src/components/CartIcon/CartIcon.js
@@ -6,7 +6,7 @@ import { toggleCartHidden } from "../../redux/cart/cartActions";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-const CartIcon = () => {
+const CartIcon = ({ toggleCartHidden }) => {
 	return (
 		<div className="cart-icon" onClick={toggleCartHidden}>
 			<ShoppingIcon className="shopping-icon">
