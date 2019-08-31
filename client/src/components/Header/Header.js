1b97e8464803b5de8b7653970d8995eaf8ea0c52
Ryan Silva <RyandotSilva@gmail.com>
2019-08-31 11:46:32 -0400

    Add CartDropdown into Header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 
 import { auth } from "../../firebase/firebaseUtils";
 import CartIcon from "../CartIcon/CartIcon";
+import CartDropdown from "../CartDropdown/CartDropdown";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
@@ -32,6 +33,7 @@ const Header = ({ currentUser }) => (
 			)}
 			<CartIcon></CartIcon>
 		</div>
+		<CartDropdown />
 	</div>
 );
 
