065b54916c5a8aab543f8082cd537c474626c86a
Ryan Silva <RyandotSilva@gmail.com>
2019-08-31 13:31:02 -0400

    Add map to display cart items

--- client/src/components/CartDropdown/CartDropdown.js
+++ client/src/components/CartDropdown/CartDropdown.js
@@ -9,6 +9,9 @@ const CartDropdown = () => {
 	return (
 		<div className="cart-dropdown">
 			<div className="cart-items">
+				{cartItems.map(cartItem => (
+					<CartItem key={cartItem.id} item={cartItem} />
+				))}
 				<CustomButton>GO TO CHECKOUT</CustomButton>
 			</div>
 		</div>
