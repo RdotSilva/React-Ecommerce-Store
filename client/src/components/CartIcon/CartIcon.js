f197f1517910d2af7ee761ad23abd3725a9f64d2
Ryan Silva <RyandotSilva@gmail.com>
2019-08-31 11:58:47 -0400

    Add mapDispatchToProps

--- client/src/components/CartIcon/CartIcon.js
+++ client/src/components/CartIcon/CartIcon.js
@@ -1,5 +1,9 @@
 import "./CartIconStyles.scss";
 import React from "react";
+import { connect } from "react-redux";
+
+import { toggleCartHidden } from "../../redux/cart/cartActions";
+
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 const CartIcon = () => {
@@ -11,5 +15,9 @@ const CartIcon = () => {
 		</div>
 	);
 };
+
+const mapDispatchToProps = dispatch => ({
+	toggleCartHidden: () => dispatch(toggleCartHidden())
+});
 
 export default CartIcon;
