a6f005df49ed07879d0f1c6e7d5ae36dfe29269e
Ryan Silva <RyandotSilva@gmail.com>
2019-09-01 12:20:11 -0400

    Add map to show cart items & total

--- client/src/pages/Checkout/Checkout.js
+++ client/src/pages/Checkout/Checkout.js
@@ -29,6 +29,10 @@ const Checkout = ({ cartItems, total }) => {
 					<span>Remove</span>
 				</div>
 			</div>
+			{cartItems.map(cartItem => cartItem.name)}
+			<div className="total">
+				<span>TOTAL: ${total}</span>
+			</div>
 		</div>
 	);
 };
