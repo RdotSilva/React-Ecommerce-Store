1d961595339ca177ef49f984910cb51cc3ff7395
Ryan Silva <RyandotSilva@gmail.com>
2019-09-01 11:02:18 -0400

    Add selectCartHidden selector

--- client/src/redux/cart/cartSelectors.js
+++ client/src/redux/cart/cartSelectors.js
@@ -1,10 +1,16 @@
 import { createSelector } from "reselect";
+import CartDropdown from "../../components/CartDropdown/CartDropdown";
 
 const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
 	[selectCart],
 	cart => cart.cartItems
+);
+
+export const selectCartHidden = createSelector(
+	[selectCart],
+	cart => cart.hidden
 );
 
 export const selectCartItemsCount = createSelector(
