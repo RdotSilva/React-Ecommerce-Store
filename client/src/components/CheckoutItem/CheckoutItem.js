817a516b13cb6e1b93eb4fb67a33744f0932d2da
Ryan Silva <RyandotSilva@gmail.com>
2019-09-03 17:57:16 -0400

    Add image inside of container

--- client/src/components/CheckoutItem/CheckoutItem.js
+++ client/src/components/CheckoutItem/CheckoutItem.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./CheckoutItemStyles.scss";
 
 const CheckoutItem = () => {
-	return <div></div>;
+	return (
+		<div className="checkout-item">
+			<div className="image-container">
+				<img alt="item" />
+			</div>
+		</div>
+	);
 };
 
 export default CheckoutItem;
