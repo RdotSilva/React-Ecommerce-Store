1a2156146ee2da40ba7414a8c2d0c59cc3abfab1
Ryan Silva <RyandotSilva@gmail.com>
2019-09-03 18:35:19 -0400

    Add removeItemFromCart utility

--- client/src/redux/cart/cartUtils.js
+++ client/src/redux/cart/cartUtils.js
@@ -16,3 +16,21 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 	// If cart item doesn't exist add it and init quantity to 1.
 	return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+	const existingCartItem = cartItems.find(
+		cartItem => cartItem.id === cartItemToRemove.id
+	);
+
+	// If quantity is 1 remove the item from cart.
+	if (existingCartItem.quantity === 1) {
+		return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+	}
+
+	// If quantity is greater than 1, decrease quantity by 1.
+	return cartItems.map(cartItem =>
+		cartItem.id === cartItemToRemove.id
+			? { ...cartItem, quantity: cartItem.quantity - 1 }
+			: cartItem
+	);
+};
