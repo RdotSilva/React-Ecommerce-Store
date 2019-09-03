cd823d1efd1c5e0bb11dd0706778e137ee177ba2
Ryan Silva <RyandotSilva@gmail.com>
2019-09-03 18:37:58 -0400

    Add onClick handlers to quantity arrows

--- client/src/components/CheckoutItem/CheckoutItem.js
+++ client/src/components/CheckoutItem/CheckoutItem.js
@@ -9,7 +9,7 @@ import {
 
 import "./CheckoutItemStyles.scss";
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
 	const { name, imageUrl, price, quantity } = cartItem;
 	return (
 		<div className="checkout-item">
@@ -18,9 +18,13 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 			</div>
 			<span className="name">{name}</span>
 			<span className="quantity">
-				<div className="arrow">&#10094;</div>
+				<div className="arrow" onClick={() => removeItem(cartItem)}>
+					&#10094;
+				</div>
 				<span className="value">{quantity}</span>
-				<div className="arrow">&#10095;</div>
+				<div className="arrow" onClick={() => addItem(cartItem)}>
+					&#10095;
+				</div>
 			</span>
 			<span className="price">{price}</span>
 			<div className="remove-button" onClick={() => clearItem(cartItem)}>
