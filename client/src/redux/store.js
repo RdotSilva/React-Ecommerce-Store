cff6219759a832f2b17d2d37c20a84e3d808a0d6
Ryan Silva <RyandotSilva@gmail.com>
2019-09-04 20:18:47 -0400

    Add persistor

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./rootReducer";
@@ -6,5 +7,7 @@ import rootReducer from "./rootReducer";
 const middleware = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middleware));
+
+const persistor = persistStore(store);
 
 export default store;
