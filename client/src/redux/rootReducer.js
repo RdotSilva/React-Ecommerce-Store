e1d193b64e5d63f99223e2754dde284126ff55a7
Ryan Silva <RyandotSilva@gmail.com>
2019-09-04 20:23:24 -0400

    Add persistConfig

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -1,7 +1,15 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage/";
 
 import userReducer from "./user/userReducer";
 import cartReducer from "./cart/cartReducer";
+
+const persistConfig = {
+	key: "root",
+	storage,
+	whitelist: ["cart"]
+};
 
 export default combineReducers({
 	user: userReducer,
