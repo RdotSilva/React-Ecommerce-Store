f163987ceba9a6b53c20596ca3a3b4b940bfb4ff
Ryan Silva <RyandotSilva@gmail.com>
2019-09-05 18:19:49 -0400

    Fix connect typo

--- client/src/pages/Shop/Shop.js
+++ client/src/pages/Shop/Shop.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from "react-redux;";
+import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import CollectionPreview from "../../components/CollectionPreview/CollectionPreview";
