0829747fe82a89325bb70aec89fdd509b166b23a
Ryan Silva <RyandotSilva@gmail.com>
2019-09-05 18:40:25 -0400

    Add CollectionsOverview component

---
+++ client/src/components/CollectionsOverview/CollectionsOverview.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import "./CollectionsOverviewStyles.scss";
+
+const CollectionsOverview = () => {
+	return <div></div>;
+};
+
+export default CollectionsOverview;
