2675cbba95db17c10ac43ee66c13c3722d47953f
Ryan Silva <RyandotSilva@gmail.com>
2019-09-05 19:12:10 -0400

    Add connect

--- client/src/pages/Collection/Collection.js
+++ client/src/pages/Collection/Collection.js
@@ -7,8 +7,8 @@ import CollectionItem from "../../components/CollectionItem/CollectionItem";
 
 import "./CollectionStyles.scss";
 
-const Collection = ({ match }) => {
-	console.log(match.params.collectionId);
+const Collection = ({ collection }) => {
+	console.log(collection);
 	return (
 		<div className="collection-page">
 			<h2>Collection Page</h2>
@@ -20,4 +20,4 @@ const mapStateToProps = (state, ownProps) => ({
 	collection: selectCollection(ownProps.match.params.collectionId)(state)
 });
 
-export default Collection;
+export default connect(mapStateToProps)(Collection);
