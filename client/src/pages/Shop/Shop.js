8bc146ae5dbdd36e9d61ddc5f5a83d6833eb261e
Ryan Silva <RyandotSilva@gmail.com>
2019-09-05 18:57:37 -0400

    Add category id route

--- client/src/pages/Shop/Shop.js
+++ client/src/pages/Shop/Shop.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import CollectionsOverview from "../../components/CollectionsOverview/CollectionsOverview";
+import Category from "../../pages/Category/Category";
 
 const Shop = ({ match }) => {
 	return (
 		<div className="shop-page">
 			<Route exact path={`${match.path}`} component={CollectionsOverview} />
+			<Route path={`${match.path}/:categoryId`} component={Category} />
 		</div>
 	);
 };
