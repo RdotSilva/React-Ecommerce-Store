ee3a44525a87b3c6d3746dafdfcbcf6d07193374
Ryan Silva <RyandotSilva@gmail.com>
2019-09-05 18:42:10 -0400

    Add map method for collection preview

--- client/src/components/CollectionsOverview/CollectionsOverview.js
+++ client/src/components/CollectionsOverview/CollectionsOverview.js
@@ -2,10 +2,21 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
+import { CollectionPreview } from "../CollectionPreview/CollectionPreview";
+
 import "./CollectionsOverviewStyles.scss";
 
-const CollectionsOverview = () => {
-	return <div></div>;
+const CollectionsOverview = ({ collections }) => {
+	return (
+		<div className="collections-overview">
+			{collections.map(({ id, ...otherCollectionProps }) => (
+				<CollectionPreview
+					key={id}
+					{...otherCollectionProps}
+				></CollectionPreview>
+			))}
+		</div>
+	);
 };
 
 export default CollectionsOverview;
