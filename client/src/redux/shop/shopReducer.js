14afcbd241d4111ce419371ab5ac76d58d74dd64
Ryan Silva <RyandotSilva@gmail.com>
2019-09-05 18:09:49 -0400

    Add shopReducer

--- client/src/redux/shop/shopReducer.js
+++ client/src/redux/shop/shopReducer.js
@@ -3,3 +3,12 @@ import SHOP_DATA from "./ShopData";
 const initialState = {
 	collections: SHOP_DATA
 };
+
+const shopReducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+export default shopReducer;
