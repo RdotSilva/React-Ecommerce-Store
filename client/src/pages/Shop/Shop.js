3f8ce7f60da5f8293d828bad5d4a9678f96ce101
Ryan Silva <RyandotSilva@gmail.com>
2019-09-05 18:12:47 -0400

    Convert shop to functional component

--- client/src/pages/Shop/Shop.js
+++ client/src/pages/Shop/Shop.js
@@ -1,31 +1,18 @@
-import React, { Component } from "react";
-import SHOP_DATA from "./ShopData";
+import React from "react";
 
 import CollectionPreview from "../../components/CollectionPreview/CollectionPreview";
 
-class Shop extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			collections: SHOP_DATA
-		};
-	}
-
-	render() {
-		const { collections } = this.state;
-
-		return (
-			<div className="shop-page">
-				{collections.map(({ id, ...otherCollectionProps }) => (
-					<CollectionPreview
-						key={id}
-						{...otherCollectionProps}
-					></CollectionPreview>
-				))}
-			</div>
-		);
-	}
-}
+const Shop = ({ collections }) => {
+	return (
+		<div className="shop-page">
+			{collections.map(({ id, ...otherCollectionProps }) => (
+				<CollectionPreview
+					key={id}
+					{...otherCollectionProps}
+				></CollectionPreview>
+			))}
+		</div>
+	);
+};
 
 export default Shop;
