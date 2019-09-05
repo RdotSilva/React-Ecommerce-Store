5ac7cc1feccc66a60ec9ca6fe957ea9db7c00d1d
Ryan Silva <RyandotSilva@gmail.com>
2019-09-05 18:54:53 -0400

    Add Category component

---
+++ client/src/pages/Category/Category.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CollectionItem from "../../components/CollectionItem/CollectionItem";
+
+import "./CategoryStyles.scss";
+
+const Category = () => {
+	return (
+		<div className="category">
+			<h2>Category Page</h2>
+		</div>
+	);
+};
+
+export default Category;
