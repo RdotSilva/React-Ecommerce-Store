290e3c9751e043184bb8ccd9a605204cccd0de2c
Ryan Silva <RyandotSilva@gmail.com>
2019-09-06 16:26:09 -0400

    Add test card info

--- client/src/pages/Checkout/Checkout.js
+++ client/src/pages/Checkout/Checkout.js
@@ -37,6 +37,11 @@ const Checkout = ({ cartItems, total }) => {
 			))}
 			<div className="total">
 				<span>TOTAL: ${total}</span>
+				<div className="test-warning">
+					*USE THIS CREDIT CARD FOR TESTING PAYMENTS*
+					<br />
+					4242 4242 4242 4242 - EXP: 01/20 - CVV: 123
+				</div>
 				<StripeButton price={total} />
 			</div>
 		</div>
