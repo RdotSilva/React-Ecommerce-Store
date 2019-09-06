4b259624b085abdd3c4ddd4b98756355c79d8d79
Ryan Silva <RyandotSilva@gmail.com>
2019-09-06 15:33:08 -0400

    Add items map inside collection

--- client/src/pages/Collection/Collection.js
+++ client/src/pages/Collection/Collection.js
@@ -8,10 +8,15 @@ import CollectionItem from "../../components/CollectionItem/CollectionItem";
 import "./CollectionStyles.scss";
 
 const Collection = ({ collection }) => {
-	console.log(collection);
+	const { title, items } = collection;
 	return (
 		<div className="collection-page">
-			<h2>Collection Page</h2>
+			<h2 className="title">{title}</h2>
+			<div className="items">
+				{items.map(item => (
+					<CollectionItem key={item.id} item={item} />
+				))}
+			</div>
 		</div>
 	);
 };
