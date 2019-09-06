9ff0f0841ccd880e34df510d84b3a38733c994dc
Ryan Silva <RyandotSilva@gmail.com>
2019-09-06 15:42:52 -0400

    Remove collection id map

--- client/src/redux/shop/shopSelectors.js
+++ client/src/redux/shop/shopSelectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect";
 
-const collectionIdMap = {
-	hats: 1,
-	sneakers: 2,
-	jackets: 3,
-	womens: 4,
-	mens: 5
-};
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -15,11 +7,8 @@ export const selectCollections = createSelector(
 	shop => shop.collections
 );
 
-export const selectCollection = collectionUrlparam =>
+export const selectCollection = collectionUrlParam =>
 	createSelector(
 		[selectCollections],
-		collections =>
-			collections.find(
-				collection => collection.id === collectionIdMap[collectionUrlparam]
-			)
+		collections => collections[collectionUrlParam]
 	);
